f3ec4c8ea71b596515b6afa9c83db364dd4b677f
himuxxx <141924215+himuxxx@users.noreply.github.com>
2026-08-27 14:39:54 +0600

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -96,7 +96,10 @@ async def youtube_dl_call_back(bot, update):
         "-f", f"{youtube_dl_format}bestvideo+bestaudio/best",
         "--hls-prefer-ffmpeg",
         "--cookies", cookies_file,
-        "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "--add-header", "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "--add-header", "Accept-Language:en-US,en;q=0.5",
+        "--add-header", "Sec-Fetch-Mode:navigate",
         youtube_dl_url,
         "-o", download_directory
     ]
@@ -111,7 +114,10 @@ async def youtube_dl_call_back(bot, update):
             "--cookies", cookies_file,
             "--audio-format", youtube_dl_ext,
             "--audio-quality", youtube_dl_format,
-            "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "--add-header", "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "--add-header", "Accept-Language:en-US,en;q=0.5",
+            "--add-header", "Sec-Fetch-Mode:navigate",
             youtube_dl_url,
             "-o", download_directory
         ]
